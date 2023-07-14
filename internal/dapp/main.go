6477e5f08fcd60bafb8e1009c642ce38c67ee498
FDKevin <fdkevinzero@gmail.com>
2023-07-14 18:42:28 +0800

    feat: disable check ceramic admin access

--- internal/dapp/main.go
+++ internal/dapp/main.go
@@ -1,10 +1,8 @@
 package dapp
 
 import (
-	"context"
 	"log"
 	"os"
-	"time"
 
 	"github.com/dataverse-os/dapp-backend/ceramic"
 )
@@ -21,9 +19,9 @@ func InitCeramicSession() {
 	if CeramicSession, err = ceramic.NewSession(ceramicURL, ceramicAdminKey); err != nil {
 		log.Fatalln(err)
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-	if err = ceramic.Default.CheckAdminAccess(ctx, CeramicSession); err != nil {
-		log.Fatalf("failed to parse ceramic url with error: %s", err)
-	}
+	// ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	// defer cancel()
+	// if err = ceramic.Default.CheckAdminAccess(ctx, CeramicSession); err != nil {
+	// 	log.Fatalf("failed to check ceramic admin access with error: %s", err)
+	// }
 }
