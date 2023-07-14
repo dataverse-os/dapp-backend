d77e04d2ac77b5c7f5a7c7aea8b73c50afce255e
FDKevin <fdkevinzero@gmail.com>
2023-07-14 14:39:31 +0800

    fix: get env

--- internal/dapp/main.go
+++ internal/dapp/main.go
@@ -10,13 +10,15 @@ import (
 )
 
 var (
-	CeramicSession ceramic.Session
-	IsSandbox      = os.Getenv("IS_SANDBOX") == "TRUE"
+	CeramicSession  ceramic.Session
+	IsSandbox       = os.Getenv("IS_SANDBOX") == "TRUE"
+	ceramicURL      = os.Getenv("CERAMIC_URL")
+	ceramicAdminKey = os.Getenv("CERAMIC_ADMIN_KEY")
 )
 
 func InitCeramicSession() {
 	var err error
-	if CeramicSession, err = ceramic.NewSession(os.Getenv("CERAMIC_URL"), os.Getenv("DID_PRIVATE_KEY")); err != nil {
+	if CeramicSession, err = ceramic.NewSession(ceramicURL, ceramicAdminKey); err != nil {
 		log.Fatalln(err)
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
